c8864b029c813a8ae065b5f8536a9b783f06b80c
hasansino <4024450+hasansino@users.noreply.github.com>
2021-02-23 14:07:42 +0200

    graph BFS/DFS and shortest path search

--- structures/graph/graph.go
+++ structures/graph/graph.go
@@ -1,15 +1,18 @@
 package graph
 
+import (
+	"github.com/hasansino/gobasics/structures/queue"
+	"github.com/hasansino/gobasics/structures/stack"
+)
+
 // https://en.wikipedia.org/wiki/Graph_theory
 // https://en.wikipedia.org/wiki/Graph_(abstract_data_type)
 // https://afteracademy.com/blog/introduction-to-graph-in-programming
-// https://afteracademy.com/blog/graph-traversal-breadth-first-search
-// https://afteracademy.com/blog/graph-traversal-depth-first-search
 
 // Graph is ... well, a graph
 type Graph struct {
 	size  int
-	nodes []Node
+	nodes []*Node
 	edges [][]*Edge // Adjacency Matrix
 }
 
@@ -31,35 +34,43 @@ func NewGraph(size int) *Graph {
 	}
 	return &Graph{
 		size:  size,
-		nodes: make([]Node, 0, size),
+		nodes: make([]*Node, 0, size),
 		edges: edges,
 	}
 }
 
 // Eccentricity returns maximum distance between given n to all others
-func (g *Graph) Eccentricity(n int) int { return 0 }
+func (g *Graph) Eccentricity(n int) int { return 0 } // @TODO
 
 // Radius is minimum eccentricity of a graph
-func (g *Graph) Radius() int { return 0 }
+func (g *Graph) Radius() int { return 0 } // @TODO
 
 // Diameter is maximum eccentricity of a graph
-func (g *Graph) Diameter() int { return 0 }
+func (g *Graph) Diameter() int { return 0 } // @TODO
 
 // CentralPoint is node whose eccentricity is equal to radius
-func (g *Graph) CentralPoint() int { return 0 }
+func (g *Graph) CentralPoint() int { return 0 } // @TODO
 
 // Circumference returns number of edges in longest path of graph.
-func (g *Graph) Circumference() int { return 0 }
+func (g *Graph) Circumference() int { return 0 } // @TODO
 
 // InsertNode into graph with given value
 // Returns index of insertion
 func (g *Graph) InsertNode(v interface{}) int {
-	g.nodes = append(g.nodes, Node{value: v})
+	g.nodes = append(g.nodes, &Node{value: v})
 	return len(g.nodes)
 }
 
 // RemoveNode from graph
-func (g *Graph) RemoveNode(i int) {}
+func (g *Graph) RemoveNode(i int) {
+	g.nodes[i] = nil
+	for j := 0; j < g.size; j++ {
+		g.edges[i][j] = nil
+	}
+	for j := 0; j < g.size; j++ {
+		g.edges[j][i] = nil
+	}
+}
 
 // CreateEdge between i and j with weight w and direction d
 func (g *Graph) CreateEdge(i, j, w int) {
@@ -73,5 +84,127 @@ func (g *Graph) RemoveEdge(i, j int) {
 
 // Adjacent tests if i and j have an edge
 func (g *Graph) Adjacent(i, j int) bool {
-	return g.edges[i][j] != nil
+	return g.edges[i][j] != nil || g.edges[j][i] != nil
+}
+
+// https://en.wikipedia.org/wiki/Breadth-first_search
+// https://afteracademy.com/blog/graph-traversal-breadth-first-search
+
+// BreathFirstSearch of a graph
+func (g *Graph) BreathFirstSearch(v interface{}) bool {
+	if g.nodes[0] == nil {
+		return false // empty graph
+	}
+
+	var (
+		q       = queue.NewLLQueue(g.size)
+		visited = make(map[int]bool, g.size)
+	)
+
+	if err := q.Enqueue(0); err != nil {
+		return false
+	}
+
+	for !q.Empty() {
+		n := q.Dequeue().(int)
+		if visited[n] {
+			continue
+		}
+		if g.nodes[n].value == v {
+			return true
+		}
+		visited[n] = true
+		for j := 0; j < g.size; j++ {
+			if g.edges[n][j] != nil {
+				if err := q.Enqueue(j); err != nil {
+					return false
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+// https://en.wikipedia.org/wiki/Depth-first_search
+// https://afteracademy.com/blog/graph-traversal-depth-first-search
+
+// DepthFirstSearch of graph
+func (g *Graph) DepthFirstSearch(v interface{}) bool {
+	if g.nodes[0] == nil {
+		return false // empty graph
+	}
+
+	var (
+		s       = stack.NewLLStack()
+		visited = make(map[int]bool, g.size)
+	)
+
+	s.Push(0)
+
+	for !s.Empty() {
+		n := s.Pop().(int)
+		if visited[n] {
+			continue
+		}
+		if g.nodes[n].value == v {
+			return true
+		}
+		visited[n] = true
+		for j := 0; j < g.size; j++ {
+			if g.edges[n][j] != nil {
+				s.Push(j)
+			}
+		}
+	}
+
+	return false
+}
+
+// ShortestPathQueueItem with path already traversed for backtracking
+type ShortestPathQueueItem struct {
+	path []int
+	node int
+}
+
+// ShortestPath from root node to node with given value
+func (g *Graph) ShortestPath(v interface{}) []int {
+	if g.nodes[0] == nil {
+		return nil // empty graph
+	}
+
+	var (
+		q       = queue.NewLLQueue(g.size)
+		visited = make(map[int]bool, g.size)
+	)
+
+	if err := q.Enqueue(ShortestPathQueueItem{
+		node: 0, path: make([]int, 0),
+	}); err != nil {
+		return nil
+	}
+
+	for !q.Empty() {
+		qi := q.Dequeue().(ShortestPathQueueItem)
+		if visited[qi.node] {
+			continue
+		}
+		if g.nodes[qi.node].value == v {
+			qi.path = append(qi.path, qi.node)
+			return qi.path
+		}
+		visited[qi.node] = true
+		for j := 0; j < g.size; j++ {
+			if g.edges[qi.node][j] != nil {
+				if err := q.Enqueue(ShortestPathQueueItem{
+					node: j,
+					path: append(qi.path, qi.node),
+				}); err != nil {
+					return nil
+				}
+			}
+		}
+	}
+
+	return nil
 }
--- structures/graph/graph_test.go
+++ structures/graph/graph_test.go
@@ -1,6 +1,10 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var (
 	//        234
@@ -16,13 +20,21 @@ var (
 		i, j, w int
 	}{
 		{0, 1, 1},
+		{1, 0, 1},
 		{1, 5, 1},
+		{5, 1, 1},
 		{5, 6, 1},
+		{6, 5, 1},
 		{0, 2, 1},
+		{2, 0, 1},
 		{1, 3, 1},
+		{3, 1, 1},
 		{6, 3, 1},
+		{3, 6, 1},
 		{2, 4, 1},
+		{4, 2, 1},
 		{3, 4, 1},
+		{4, 3, 1},
 	}
 )
 
@@ -34,4 +46,61 @@ func TestGraph_Build(t *testing.T) {
 	for _, v := range testEdges {
 		g.CreateEdge(v.i, v.j, v.w)
 	}
+}
+
+func TestGraph_BFS(t *testing.T) {
+	g := NewGraph(len(testNodes))
+	for _, v := range testNodes {
+		g.InsertNode(v)
+	}
+	for _, v := range testEdges {
+		g.CreateEdge(v.i, v.j, v.w)
+	}
+
+	assert.True(t, g.BreathFirstSearch(4))
+	assert.True(t, g.BreathFirstSearch(6))
+	assert.True(t, g.BreathFirstSearch(33))
+	assert.True(t, g.BreathFirstSearch(654))
+	assert.True(t, g.BreathFirstSearch(2))
+	assert.True(t, g.BreathFirstSearch(234))
+	assert.True(t, g.BreathFirstSearch(546))
+
+	assert.False(t, g.BreathFirstSearch(0))
+	assert.False(t, g.BreathFirstSearch(1))
+	assert.False(t, g.BreathFirstSearch(999))
+}
+
+func TestGraph_DFS(t *testing.T) {
+	g := NewGraph(len(testNodes))
+	for _, v := range testNodes {
+		g.InsertNode(v)
+	}
+	for _, v := range testEdges {
+		g.CreateEdge(v.i, v.j, v.w)
+	}
+
+	assert.True(t, g.DepthFirstSearch(4))
+	assert.True(t, g.DepthFirstSearch(6))
+	assert.True(t, g.DepthFirstSearch(33))
+	assert.True(t, g.DepthFirstSearch(654))
+	assert.True(t, g.DepthFirstSearch(2))
+	assert.True(t, g.DepthFirstSearch(234))
+	assert.True(t, g.DepthFirstSearch(546))
+
+	assert.False(t, g.DepthFirstSearch(0))
+	assert.False(t, g.DepthFirstSearch(1))
+	assert.False(t, g.DepthFirstSearch(999))
+}
+
+func TestGraph_ShortestPath(t *testing.T) {
+	g := NewGraph(len(testNodes))
+	for _, v := range testNodes {
+		g.InsertNode(v)
+	}
+	for _, v := range testEdges {
+		g.CreateEdge(v.i, v.j, v.w)
+	}
+	assert.Equal(t, []int{0}, g.ShortestPath(4))
+	assert.Equal(t, []int{0, 2, 4}, g.ShortestPath(2))
+	assert.Equal(t, []int{0, 1, 3, 6}, g.ShortestPath(546))
 }
